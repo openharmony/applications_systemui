cdee8fedd2d7d2c75ca689bb48c6c0bcfc52cb27
match <zhouqingfeng2@huawei.com>
2022-05-24 14:28:10 +0800

    systemui 竖屏适配
    Signed-off-by: match <zhouqingfeng2@huawei.com>

--- product/phone/statusbar/src/main/ets/ServiceExtAbility/ServiceExtAbility.ts
+++ product/phone/statusbar/src/main/ets/ServiceExtAbility/ServiceExtAbility.ts
@@ -18,6 +18,7 @@ import Log from '../../../../../../../common/src/main/ets/default/Log'
 import WindowManager, { WindowType } from '../../../../../../../common/src/main/ets/default/WindowManager'
 import AbilityManager from '../../../../../../../common/src/main/ets/default/abilitymanager/abilityManager'
 import StatusBarConfiguration from '../../../../../../../features/statusbarcomponent/src/main/ets/com/ohos/common/StatusBarConfiguration';
+import CommonStyleConfiguration from '../../../../../../../common/src/main/ets/default/StyleConfiguration'
 
 const TAG = "StatusBar_ServiceExtAbility"
 
@@ -30,11 +31,13 @@ class ServiceExtAbility extends ServiceExtension {
         AbilityManager.setAbilityData(AbilityManager.ABILITY_NAME_STATUS_BAR, 'config', configInfo)
         Log.showDebug(TAG, `onCreate, configInfo: ${JSON.stringify(configInfo)}`)
 
+        let style:any = CommonStyleConfiguration.getCommonStyle()
+        let deviceTypeInfo = style.deviceTypeInfo
         let statusBarRect = {
             left: configInfo.xCoordinate,
             top: configInfo.yCoordinate,
             width: configInfo.realWidth,
-            height: configInfo.realHeight
+            height: deviceTypeInfo == 'phone' ? configInfo.realHeight : configInfo.realHeight/3
         }
         WindowManager.createWindow(this.context, WindowType.STATUS_BAR, statusBarRect, "pages/index").then(() =>
             WindowManager.showWindow(WindowType.STATUS_BAR)
