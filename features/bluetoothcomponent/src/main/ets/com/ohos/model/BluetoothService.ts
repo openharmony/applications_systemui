7a1c438103202e8ebc252134ffa7cba1c4b21405
lizhi <lizhi140@huawei.com>
2022-08-03 10:52:15 +0800

    [systemui]添加悬浮通知动画和设置蓝牙扫描模式
    Signed-off-by: lizhi <lizhi140@huawei.com>

--- features/bluetoothcomponent/src/main/ets/com/ohos/model/BluetoothService.ts
+++ features/bluetoothcomponent/src/main/ets/com/ohos/model/BluetoothService.ts
@@ -49,6 +49,7 @@ export class BluetoothService {
       if (this.mIsBluetoothOpen != isOpen) {
         Log.showInfo(TAG, `state change: ${isOpen}`);
         this.mIsBluetoothOpen = isOpen;
+        bluetooth.setBluetoothScanMode(bluetooth.ScanMode.SCAN_MODE_CONNECTABLE_GENERAL_DISCOVERABLE, 0);
         this.mListener?.updateState(this.mIsBluetoothOpen);
       }
     });
