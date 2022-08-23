cf4dc17a94b5dbaef5a043b3b094bdba64685c1e
lijinfengde123 <lijinfeng26@huawei.com>
2022-08-23 16:21:46 +0800

    增加通知删除类型
    Signed-off-by: lijinfengde123 <lijinfeng26@huawei.com>

--- features/noticeitem/src/main/ets/com/ohos/noticeItem/model/NotificationManager.ts
+++ features/noticeitem/src/main/ets/com/ohos/noticeItem/model/NotificationManager.ts
@@ -50,9 +50,10 @@ export default class NotificationManager {
     Notification.removeAll(callback);
   }
 
-  static remove(tag: string, hashCode, callback): void {
+  static remove(tag: string, hashCode,callback, isClickItem?: boolean): void {
     Log.showInfo(TAG, `remove from: ${tag}`);
-    Notification.remove(hashCode, callback);
+    var clickValue = isClickItem == true ? Notification.RemoveReason.CLICK_REASON_REMOVE : Notification.RemoveReason.CANCEL_REASON_REMOVE;
+    Notification.remove(hashCode, clickValue, callback);
   }
 
   static getAllActiveNotifications(tag: string, callback): void {
