a13fa55372fc7027e95deb440723b07445a6bb24
gaochao <gaochao55@huawei.com>
2022-09-26 15:04:26 +0800

    [systemui]适配组件权限访问新规则
    Signed-off-by: gaochao <gaochao55@huawei.com>

--- features/volumepanelcomponent/src/main/ets/com/ohos/viewmodel/VolumePanelVM.ts
+++ features/volumepanelcomponent/src/main/ets/com/ohos/viewmodel/VolumePanelVM.ts
@@ -114,7 +114,7 @@ export class VolumePanelVM {
 
   updateVolumeInfo(volumeInfo: VolumeInfo): void {
     Log.showInfo(TAG, `updateVolumeInfo, volumeInfo: ${JSON.stringify(volumeInfo)} , mCurrentAudioVolumeType: ${this.mCurrentAudioVolumeType}`);
-    if (!volumeInfo.updateUi) {
+    if (!volumeInfo.updateUi && this.mIsMute.get() == volumeInfo.isMute) {
       return;
     }
     let volumeType = volumeInfo.volumeType;
