8b692782c9675d826a230f72bf2018f21e75bb4e
杨江涛 <yangjiangtao001@chinasoftinc.com>
2024-04-09 10:18:48 +0800

    fixed daf5425 from https://gitee.com/yang-jiangtao001/applications_systemui/pulls/464 4月1日进行12小时测试，出现jscrash问题，应用：com.ohos.systemu
    Signed-off-by: 杨江涛 <yangjiangtao001@chinasoftinc.com>

--- features/airplanecomponent/src/main/ets/default/viewmodel/AirplaneVM.ts
+++ features/airplanecomponent/src/main/ets/default/viewmodel/AirplaneVM.ts
@@ -39,14 +39,14 @@ class AirplaneVM implements AirplaneServiceListener {
   constructor() {
     this.context = AbilityManager.getContext(AbilityManager.getContextName(AbilityManager.ABILITY_NAME_CONTROL_PANEL));
     if (this.context == undefined || this.context == null) {
-      Log.showInfo(TAG, `constructor: ${context}`);
+      Log.showInfo(TAG, `constructor: ${this.context}`);
       return;
     }
     try {
       this.mAirplaneStatus =
         settings.getValueSync(this.context, Constants.KEY_AIRPLANE_MODE_STATUS, AIRPLANE_MODE_STATUS.OFF) === AIRPLANE_MODE_STATUS.ON
     } catch (err) {
-      Log.showError(TAG, `AirplaneVM: ${context}, ${JSON.stringify(err)}`);
+      Log.showError(TAG, `AirplaneVM: ${this.context}, ${JSON.stringify(err)}`);
     }
     AppStorage.SetOrCreate('Airplane_Status', this.mAirplaneStatus);
   }
